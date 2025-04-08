069d3578573fcdf1bedc069395c4baaf19f8a67d
Parker Sims <parker4al@gmail.com>
2025-04-07 22:52:19 -0500

    Completed Exercise 2

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -5,6 +5,8 @@
 
         static void Main(string[] args)
         {
+            //Exercise 1
+            
             int favNumber = 29;
 
             Console.WriteLine("Try to guess my favorite number between 1-100.");
@@ -13,15 +15,53 @@
 
             if (userInput < favNumber)
             {
-                Console.WriteLine("Too low, try again.");
+                Console.WriteLine("Too low.");
             }
             else if (userInput > favNumber)
             {
-                Console.WriteLine("Too high, try again.");
+                Console.WriteLine("Too high.");
             }
             else
             {
                 Console.WriteLine("Congratulations! You guessed the number.");
+            }
+            
+            
+            //Exercise 2
+            
+            Console.WriteLine("What is your favorite subject?");
+            string favoriteSubject = Console.ReadLine();
+
+            switch (favoriteSubject.ToLower())
+            {
+                case "math":
+                    Console.WriteLine("You have to be really smart if you like Math");
+                    break;
+                case "science":
+                    Console.WriteLine("I think Science is awesome!");
+                    break;
+                case "history":
+                    Console.WriteLine("If you dont know your History your bound to repeat it!");
+                    break;
+                case "art":
+                    Console.WriteLine("Thats awesome! I've never been great at drawing.");
+                    break;
+                case "music":
+                    Console.WriteLine("I've always wanted to learn the piano.");
+                    break;
+                default:
+                    Console.WriteLine("Neat. Tell me why!");
+                    Console.ReadLine();
+                    Console.WriteLine("Thanks for sharing.");
+                    break;
+                
+                
+                
+                
+                
+                
+                
+                
             }
 
 
