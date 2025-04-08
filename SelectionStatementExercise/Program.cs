69ee4ff2708133dceaa4d1882d45077026d54099
Parker Sims <parker4al@gmail.com>
2025-04-07 22:31:25 -0500

    Completed Exercise

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -2,9 +2,30 @@
 {
     public class Program
     {
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int favNumber = 29;
+
+            Console.WriteLine("Try to guess my favorite number between 1-100.");
+            
+            int userInput = int.Parse(Console.ReadLine());
+
+            if (userInput < favNumber)
+            {
+                Console.WriteLine("Too low, try again.");
+            }
+            else if (userInput > favNumber)
+            {
+                Console.WriteLine("Too high, try again.");
+            }
+            else
+            {
+                Console.WriteLine("Congratulations! You guessed the number.");
+            }
+
+
+
         }
     }
 }
